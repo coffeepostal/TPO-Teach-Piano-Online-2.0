47baf17e735edf75388255f2a5e5f23c9dfe67bc
Adam Farnsworth <hello@farns.us>
2019-04-19 15:26:48 -0700

    Moved to home

--- client/src/router.js
+++ client/src/router.js
@@ -10,7 +10,7 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
+      name: 'Practice Sheets',
       component: Home
     },
     {
@@ -20,14 +20,6 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    },
-    {
-      path: '/sheet',
-      name: 'Practice Sheets',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "sheet" */ './views/PracticeSheets.vue')
     }
   ]
 })
